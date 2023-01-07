c1f991afb5cb50605de66271c6f12e920f53b20b
Cameron Crook <cameronbcrook@gmail.com>
2023-01-06 17:20:59 -0700

    complete portal page

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,5 @@ let year = date.getFullYear();
 const fullname = "Cameron Crook";
 let currentdate = document.lastModified;
 
-console.log(currentdate);
-
-document.querySelector('#footer-info').innerHTML = `&copy; ${year} ${fullname} Wyoming`
+document.querySelector('#year').innerHTML = `&copy; ${year}`
 document.querySelector('#last-updated').textContent = `Last Updated: ${currentdate}`;
