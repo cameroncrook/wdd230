b43e38d9790d16cbe1d5346abec4a741ec3656e5
Cameron Crook <cameronbcrook@gmail.com>
2023-01-27 13:39:17 -0700

    Responsive Design

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -5,7 +5,7 @@ let modifiedDate = document.lastModified;
 let date = new Date();
 let year = date.getFullYear();
 
-copyright.innerHTML = `&copy; ${year} Star Valley Chamber of Commerce`
+copyright.innerHTML = `&copy; ${year} Star Valley Chamber of Commerce`;
 lastModified.textContent = `Last Modified: ${modifiedDate}`;
 
 // Hamburger Button JS
@@ -15,3 +15,18 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener('click', () => {
     navigation.classList.toggle('responsive');
 })
+
+const timeDiv = document.querySelector('#date');
+
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentDay = currentDate.getDay();
+let dayOfMonth = currentDate.getDate();
+
+const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'];
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+currentDay = days[currentDay];
+currentMonth = months[currentMonth];
+
+timeDiv.textContent = `${currentDay}, ${dayOfMonth} ${currentMonth} ${year}`;
